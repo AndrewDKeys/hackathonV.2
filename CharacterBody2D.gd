665315238a0fa9f03a8237e709891e3d226766cb
Michael Angamarca <mangamarca@albany.edu>
2024-04-14 01:10:54 -0400

    Added running character animation

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -1,28 +1,34 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+# Reference to the AnimationTree node in your scene
+@onready var animation_tree = $AnimationTree
 
 func _physics_process(delta):
-	# Add the gravity.
+	# Add gravity
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	# Handle jump.
+	
+	# Handle jump
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	
+	# Get the input direction and handle movement
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
+		# Update velocity based on input
 		velocity.x = direction * SPEED
+		# Set the blend position for the 'walk' BlendSpace1D based on direction
+		animation_tree.set("parameters/walk/blend_position", direction)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		# Decelerate velocity towards zero
+		velocity.x = move_toward(velocity.x, 0, SPEED * delta)
+		# Set the blend position to 0 for idle animation
+		animation_tree.set("parameters/idle/blend_position", 0)
+	
+	# Handle movement
 	move_and_slide()
