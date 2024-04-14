9cab36d2f25082a3714c8eb5af93e01d9bf3d433
nickcyran <nicyran@gmail.com>
2024-04-13 20:11:36 -0400

    movement

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -1,28 +1,38 @@
 extends CharacterBody2D
 
-
+# Get the gravity from the project settings to be synced with RigidBody 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -500.0
+const WALL_SLIDE_VELOCITY = 130;
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var facing = 1;
+var total_jumps = 2;
+var jump_count = 0;
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += gravity * delta
-
+		velocity.y += (1580) * delta
+	else:
+		jump_count = 0
+		
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and jump_count < total_jumps:
 		velocity.y = JUMP_VELOCITY
-
+		jump_count += 1
+		
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	useItem()
+	
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
+		facing = direction;
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+func useItem():
+	if Input.is_action_just_pressed("shift"):
+		velocity = Vector2( facing * 720, -800)
