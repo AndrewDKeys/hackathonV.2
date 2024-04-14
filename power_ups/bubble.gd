ce7ed696b2c5fcbeb17ac79b115ce1fd40cea285
Andrew <adkeys04@gmail.com>
2024-04-14 08:51:04 -0400

    ppups

--- power_ups/bubble.gd
+++ power_ups/bubble.gd
@@ -1,13 +1,17 @@
 extends Node2D
-
+signal pick_me
 var item_type
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	item_type = "bubble"
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	pass	
+
+func _on_area_2d_body_entered(body):
+	if body is CharacterBody2D:
+		emit_signal("pick_me", self)
 	pass
