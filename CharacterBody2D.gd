64746c74ba2e4f3119da6c1102be82ba8cfb6953
nickcyran <nicyran@gmail.com>
2024-04-13 20:23:51 -0400

    blockfolder

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -5,6 +5,7 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -500.0
 const WALL_SLIDE_VELOCITY = 130;
 
+var x = false;
 var facing = 1;
 var total_jumps = 2;
 var jump_count = 0;
@@ -14,6 +15,7 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += (1580) * delta
 	else:
+		x = false
 		jump_count = 0
 		
 	# Handle jump.
@@ -21,18 +23,24 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY
 		jump_count += 1
 		
-	# Get the input direction and handle the movement/deceleration.
-	useItem()
+	# Get the input direction and handle the movement/deceleration
+	
+	x = useItem()
 	
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		facing = direction;
 		velocity.x = direction * SPEED
-	else:
+	elif not x:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		
 	move_and_slide()
 	
-func useItem():
+func useItem() -> bool:
+	var x = false
+	
 	if Input.is_action_just_pressed("shift"):
-		velocity = Vector2( facing * 720, -800)
+		velocity = Vector2(facing * 720, -800)
+		x = true
+		
+	return x
