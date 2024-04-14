897a458521cfe144b0e6ec2c0883f0887604ecee
Andrew <adkeys04@gmail.com>
2024-04-14 05:53:16 -0400

    power-up

--- Scenes/PowerUpTimer.gd
+++ Scenes/PowerUpTimer.gd
@@ -1,14 +1,15 @@
 extends Timer
 
+const DROP_HEIGHT = -70
 const POWER_UP_ODDS = 0.20
-const SUPER_POWER_UP_ODDS = 0.05
+const SUPER_POWER_UP_ODDS = 1.0
 
 var bubble
 var bomb
 
-var triple_jump
-var full_clear
-var slingshot
+var triple_jump = preload("res://power_ups/triple_jump.tscn")
+var full_clear = preload("res://power_ups/full_clear.tscn")
+var slingshot = preload("res://power_ups/sling_shot.tscn")
 
 var chance
 
@@ -21,10 +22,10 @@ func _on_timeout():
 	randomize()
 	chance = randf() # creates a num 0.0 - 1.0
 	if chance <= SUPER_POWER_UP_ODDS:
-		gen_power_up()
+		gen_super_power_up()
 		wait_time = 12
 	elif chance <= POWER_UP_ODDS:
-		gen_super_power_up()
+		gen_power_up()
 		wait_time = 12
 	else:
 		wait_time /= 2
@@ -32,10 +33,13 @@ func _on_timeout():
 func gen_power_up():
 	randomize()
 	var power_list = [bomb, bubble, bomb]
-	var power = power_list[randi() % power_list.size()]
+	var random = power_list[randi() % power_list.size()]
 
 
 func gen_super_power_up():
 	randomize()
 	var power_list = [triple_jump, full_clear, slingshot]
-	var power = power_list[randi() % power_list.size()]
+	var random = power_list[randi() % power_list.size()]
+	var power = random.instantiate()
+	power.set_global_position(Vector2(randf_range(375, 830), DROP_HEIGHT))
+	add_child(power)
